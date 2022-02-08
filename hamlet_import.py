075a2d603f75b8670282b0180b37902f69e9ddb9
sandwich <28636996+sandwichsimp@users.noreply.github.com>
2022-02-07 22:15:38 -0500

    add comments

--- hamlet_import.py
+++ hamlet_import.py
@@ -6,19 +6,20 @@ hamlet = requests.get('http://shakespeare.mit.edu/hamlet/full.html').text
 
 raw_site = BeautifulSoup(hamlet, 'html.parser')
 
-speech_by_char = {"HORATIO": [],"HAMLET": [],"OPHELIA": [],"KING CLAUDIUS": [],"LORD POLONIUS": [],"LAERTES": [],"QUEEN GERTRUDE": [],"Ghost": [],"PRINCE FORTINBRAS": [],"VOLTIMAND": [],"GUILDENSTERN": [],"ROSENCRANTZ": [],"OSRIC": [],"Captain": [], "BERNARDO": [], "REYNALDO": [], "CORNELIUS": [], "First Clown": [], "Second Clown": [], "First Priest": [], "Player King": [], "Player Queen": [], "FRANCISCO": [], "MARCELLUS": [], "LUCIANUS": [], "Messenger": [], "All": [], "Lord": []}
+speech_by_char = {"HORATIO": [],"HAMLET": [],"OPHELIA": [],"KING CLAUDIUS": [],"LORD POLONIUS": [],
+                  "LAERTES": [],"QUEEN GERTRUDE": [],"Ghost": [],"PRINCE FORTINBRAS": [],"VOLTIMAND": [],
+                  "GUILDENSTERN": [],"ROSENCRANTZ": [],"OSRIC": [],"Captain": [], "BERNARDO": [], 
+                  "REYNALDO": [], "CORNELIUS": [], "First Clown": [], "Second Clown": [], "First Priest": [], 
+                  "Player King": [], "Player Queen": [], "FRANCISCO": [], "MARCELLUS": [], "LUCIANUS": [], 
+                  "Messenger": [], "All": [], "Lord": []}
 
 chars_list = re.compile(r'(\nHORATIO\n|\nHAMLET\n|\nOPHELIA\n|\nKING CLAUDIUS\n|\nLORD POLONIUS\n|\nLAERTES\n|\nQUEEN GERTRUDE\n|\nGhost\n|\nPRINCE FORTINBRAS\n|\nVOLTIMAND\n|\nGUILDENSTERN\n|\nROSENCRANTZ\n|\nOSRIC\n|\nCaptain\n|\nBERNARDO\n|\nREYNALDO\n|\nCORNELIUS\n|\nFirst Clown\n|\nSecond Clown\n|\nFirst Priest\n|\nPlayer King\n|\nPlayer Queen\n|\nFRANCISCO\n|\nMARCELLUS\n|\nLUCIANUS\n|\nMessenger\n|\nAll\n|\nLord\n)')
 
 A = chars_list.split(raw_site.get_text())
 
-flag = True
-for i,text in enumerate(A[1:]): 
-  try: 
-    A[i+1] = A[i+1] + A[i+2]
-    A.pop(i+2)
-  except: 
-    continue
+for i,text in enumerate(A[2:len(A)/2]): 
+  A[i-1] = A[i-1] + A[i]
+  A.pop(i)
 
 for text in A[1:]: 
   start = text.index("\n")
@@ -40,6 +41,6 @@ for entry in speech_by_char.keys():
 
   speech_data.append([entry, len(speech_by_char[entry]), total_words])
   
-  print(entry + ": " + str(len(speech_by_char[entry])) + ", " + str(total_words) + ", " + str(total_words / len(speech_by_char[entry])))
+  print(entry + ": " + str(len(speech_by_char[entry])) + ", " + str(total_words) + ", " + str(round(total_words / len(speech_by_char[entry]), 2)))
 
-print(speech_data)
+#print(speech_data)
